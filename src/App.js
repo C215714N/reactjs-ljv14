9e1eb02bb78cd8413f10a41c3e4a412ae86227be
Cristian Racedo <CristianDRacedo@hotmail.com>
2022-11-28 23:20:59 -0300

    set values and posts content

--- src/App.js
+++ src/App.js
@@ -3,20 +3,20 @@ import './App.css';
 
 function App() {
   return (
-    <>
+    <main className="container-xxl row mx-auto p-4">
       <Post 
         title="Perros" 
-        content="" 
-        val={[100,120,60]} />
+        content="Llamado perro doméstico o can, y en algunos lugares coloquialmente llamado chucho, tuso, choco; es un mamífero carnívoro de la familia de los cánidos, que constituye una especie del género Canis.​​" 
+        val={[100, 120, 60]} />
       <Post 
         title="Gatos" 
-        content=""  
-        val={[150,90,85]}/>
+        content="Llamado más comúnmente gato, y de forma coloquial minino, michino, michi, micho, mizo, miz, morroño o morrongo, y algunos nombres más, es un mamífero carnívoro de la familia Felidae."  
+        val={[150, 90, 85]}/>
       <Post 
         title="Ratones" 
-        content=""  
-        val={[120,180,30]}/>
-    </>
+        content="Mus es un género de roedores miomorfos de la familia Muridae que incluye la mayoría de los roedores, si bien el nombre de ratón se usa para varias especies más pertenecientes a otros géneros."  
+        val={[120, 180, 30]}/>
+    </main>
   );
 }
 
