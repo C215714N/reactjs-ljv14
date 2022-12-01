5eb14e4b2b35276c4ef4ed4c2f5eadafaddfcf19
Cristian Racedo <CristianDRacedo@hotmail.com>
2022-12-01 14:59:17 -0300

    Organizacion de useState y definicion de estado

--- src/Post/Button.js
+++ src/Post/Button.js
@@ -1,24 +1,22 @@
 import { useState } from "react";
 
 const Button = (props) => {
-    const { className, value } = props;
+    const { className, value, text } = props;
     /* Hooks: declaracion que facilita la implementacion de estado
         state, method
     */
     const [state, setState] = useState(false);
-    const [number, setNumber] = useState(props.value || 0);
+    const [number, setNumber] = useState( value || 0);
 
-    const Eval = () => {
-        !state ? setNumber(number + 1) : setNumber(number - 1)
+    const setReaction = () => {
+        state ? setNumber(number - 1) : setNumber(number + 1)
         setState(!state);
     }
 
     return(
-        <button onClick={() => Eval()}
-            className={className}>
-            {number} {props.text}
+        <button className={ className } onClick={ setReaction }>
+            {number} { text }
         </button>
-    )
-}
+) }
 
 export default Button;
