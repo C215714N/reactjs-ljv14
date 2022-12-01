3d02ae6ea6121eda0fb6d425a15e0486e3125138
Cristian Racedo <CristianDRacedo@hotmail.com>
2022-12-01 19:48:45 -0300

    Estructura Barra de Navegacion

--- src/NavBar/Nav.js
+++ src/NavBar/Nav.js
@@ -1,9 +1,21 @@
+import logo from '../logo.svg'
+import {useState} from 'react'
 import { NavLink } from 'react-router-dom';
 
-const Nav = (props) => 
-    <ul>
+const Nav = (props) => {
+    const [menuState, setMenuState] = useState(false);
+    return (
+    <nav className='navbar navbar-dark bg-dark bg-gradient navbar-expand-md | py-0 px-3 sticky-top'>
+        <a className='navbar-brand col-sm-2'>
+            <img className='w-100 App-logo' src={logo} />
+        </a>
+        <button className='navbar-toggler navbar-toggler-icon border-0'
+            onClick={() => setMenuState(!menuState)}></button>
+        <ul className={'navbar-nav pb-2 navbar-collapse ' + ( menuState ? 'collapse' : '' ) }>
         { props.links.map( link => 
-            <li key={link.id}><NavLink to={link.to}>{link.text}</NavLink></li> 
+            <li key={link.id} className="nav-item"><NavLink className="nav-link" to={link.to}>{link.text}</NavLink></li> 
         ) }
-    </ul>
+        </ul>
+    </nav>
+    ) }
 export default Nav;
