889f8dcb24e161849ad30639e6d057f1e4f059dc
Cristian Racedo <CristianDRacedo@hotmail.com>
2022-12-02 14:22:05 -0300

    Reestructuracion de Rutas y contenido

--- src/Error.js
+++ src/Error.js
@@ -2,7 +2,7 @@ const Error = (props) =>
     <div className="card col-md-6 my-3 mx-auto">
         <h2 className="card-header">Felicitaciones <br/> Logro:{props.code}</h2>
         <p className="card-body">
-            Has desbloqueado una ruta inexistente, ganaste 100 puntos
+            Has desbloqueado una ruta inexistente, ganaste {props.points} puntos
         </p>
     </div>
 
