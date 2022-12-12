93b7758f646a86a5dcd9e641f1f71a1205569d7a
Cristian Racedo <CristianDRacedo@hotmail.com>
2022-12-12 15:50:57 -0300

    Componente de Resultados

---
+++ src/components/Results.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import Image from './Image';
+
+const Results = (props) => {
+    const { data, word } = props
+    return(
+        <main>
+            <h2>{ word.length >= 3 ? 'Resultados de ' + word : 'Gifs Mas Populares'}</h2>
+            { data.length >= 1 ? data.map(image => 
+                <Image key={ image.id } data={ image } />  ) : '' }
+        </main>
+    )
+}
+
+export default Results;
