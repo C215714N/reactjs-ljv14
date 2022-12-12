39b89230b0ad6cb154090918c465eff55caca2b9
Cristian Racedo <CristianDRacedo@hotmail.com>
2022-12-12 17:14:40 -0300

    Barra de navegacion

---
+++ src/components/Nav.jsx
@@ -0,0 +1,8 @@
+import {NavLink} from 'react-router-dom'
+const Nav = () => 
+    <nav>
+        <NavLink to="/about">Acerca de</NavLink> 
+        <NavLink to="/search">Buscar</NavLink>
+    </nav>
+
+export default Nav;
