71ef2241c58734b6d88ed03c29ac21e8d9e95257
Cristian Racedo <CristianDRacedo@hotmail.com>
2022-12-13 02:19:42 -0300

    Navegacion Material-UI Fixed

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,12 @@
-import {NavLink} from 'react-router-dom'
-const Nav = () => 
-    <nav>
-        <NavLink to="/about">Acerca de</NavLink> 
-        <NavLink to="/search">Buscar</NavLink>
-    </nav>
+import { NavLink } from 'react-router-dom';
+import { AppBar, Toolbar, Typography } from '@mui/material';
+
+const Nav = (props) => 
+    <AppBar position="fixed">
+        <Toolbar>
+            <NavLink className="link" to="/about">Acerca de</NavLink>
+            <NavLink className="link" to="/search">Buscar</NavLink>
+        </Toolbar>
+    </AppBar>
 
 export default Nav;
