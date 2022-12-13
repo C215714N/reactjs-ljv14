125b1c3daa681d809385cebcc94bfbe98418ff64
Cristian Racedo <CristianDRacedo@hotmail.com>
2022-12-13 02:22:52 -0300

    Componente Paginacion Material-UI Box

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,14 +1,13 @@
+import { Box, Chip, Typography } from '@mui/material/';
 const Pagination = (props) => {
     const {data} = props,
     current = data.offset / 10 + 1,
     total = Math.ceil(data.total_count / data.count);
-    return (  
-    <footer>
-        <ul>
-            <li>Pagina {isNaN(current) ? 0 : current} de { isNaN(total) ? 0 : total}</li>
-            <li>Total de imagenes: {data.total_count}</li>
-        </ul>
-    </footer>
+    return (
+        <Box>
+            <Typography>Pagina {isNaN(current) ? 0 : current} de { isNaN(total) ? 0 : total}</Typography>
+            <Typography>Total de imagenes: {data.total_count}</Typography>
+        </Box>
 ) }
 
 export default Pagination;
