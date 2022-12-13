3640dd7b56b60d22c059756b840815b45bcd890a
Cristian Racedo <CristianDRacedo@hotmail.com>
2022-12-13 02:18:50 -0300

    Componente About Material-UI Card

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,19 +1,20 @@
+import {
+    Card, 
+    CardHeader, 
+    CardContent, 
+    Typography 
+} from "@mui/material";
+
 const About = (props) => {
     const {autor, colaboracion, fecha } = props.data
     return ( 
-        <>
-            <h2>{autor}</h2>
-            <p>
-                {colaboracion}
-            </p>
-            <footer>
-                <address>
-                    
-                </address>
-                <p>{fecha}</p>
-            </footer>
-        </>
-    );
-}
+        <Card variant="outlined" sx={{ m: '1rem auto', maxWidth: 768 }}>
+            <CardHeader title={ autor } />
+            <CardContent>
+                <Typography variant="subtitle1">{ colaboracion }</Typography>
+                <Typography>{ fecha }</Typography>
+            </CardContent>
+        </Card>
+) }
 
 export default About;
